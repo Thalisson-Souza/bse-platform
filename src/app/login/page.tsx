6f6902c484ab55bd467f7ddcaeed6a5de3514ac7
Thalisson Souza <thalisson.souza@acad.ufsm.br>
2025-10-22 21:12:11 -0300

    initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,20 @@
+import { styleText } from "util";
+
+export default function Login(){
+    return (
+        <div className="text-center bg-black h-screen">
+
+
+            <div className="min-h-screen flex items-center justify-center bg-gray-100">
+                <div>
+                <view className="text-center">
+                    <h1 className="text-2xl text-justify text-blue-300 font-bold ">
+                        Bem vindo ao EduPlatform!
+                    </h1>
+                </view>
+                </div>
+            </div>
+
+        </div>
+    )
+}
